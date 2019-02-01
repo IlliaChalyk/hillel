fc610866ea3ce2680d356d05866693e7732cc23f
IlyaChalyk <47118443+IlyaChalyk@users.noreply.github.com>
2019-02-01 22:30:06 +0200

    Add files via upload

---
+++ EvenThenOdd.java
@@ -0,0 +1,33 @@
+package com.hillel.lesson3;
+
+public class EvenThenOdd {
+    public static void main(String[] args) {
+
+        int[] array = {2, 5, 2, 6, 3, 8, 6, 3, 7};
+        sortEvenOdd(array);
+
+    }
+
+    private static void sortEvenOdd(int[] arr) {
+
+        int[] sorted = new int[arr.length];
+        int evenCounter = 0;
+        int oddCOunter = arr.length - 1;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] % 2 == 0) {
+                sorted[evenCounter] = arr[i];
+                evenCounter++;
+            } else {
+                sorted[oddCOunter] = arr[i];
+                oddCOunter--;
+            }
+        }
+        for (int i = 0; i < sorted.length; i++) {
+            System.out.print(sorted[i] + " ");
+        }
+
+    }
+}
+
